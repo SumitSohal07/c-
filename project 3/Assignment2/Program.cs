c2b6604eece35cfc999277d96eb2f8888305c7da
SumitSohal07 <145088567+SumitSohal07@users.noreply.github.com>
2024-01-29 14:05:52 -0500

    Add files via upload
    A dental Equipment Company pays its sales employees on a commission basis.
    The monthly salary of the sales employee is $1000 plus 10% of their gross sales for that month. For example, a salesperson who sells $10000 worth of Diamond instruments for dentists in a month receives 1000 plus 10% of $10000, or a total of $2000. 
    
    here is  a program that uses a while statement to input each salesperson’s gross sales for the past 12 month and calculates and displays that salesperson’s earnings per month and show the total at the end as well. To stop the program the user type -1 and display the message "Goodbye!".

---
+++ project 3/Assignment2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Assignment2
+{
+    static void Main(string[] args)
+    {
+        //Declaration of variables
+         const float salary = 1000;
+        float t_salary;
+        bool exitRequested = false;
+        int totalEarnings = 0;
+        float gross_tax = 0.1F;
+
+        //Message will be displayed to exit the program or continue with the program
+        while (!exitRequested)
+        {
+            Console.WriteLine("Enter 1 to exit, or any other number to continue:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput == 1)
+            {
+                Console.WriteLine("Goodbye! Exiting the program.");
+                exitRequested = true;
+            }
+            else
+            {
+                Console.Write("Enter the number of employees: \n");
+                int emp = Convert.ToInt32(Console.ReadLine());
+                //int[] employees = new int[emp];
+
+                //for loop to print the number of employees again and again
+                for (int i = 1; i <= emp; ++i)
+                {
+                    Console.Write($"Gross sales of employee {i} for the 12 months are:\n \n");
+
+
+                    Console.WriteLine("****************************\n\n");
+
+                    int totalGrossSales = 0;
+
+                    //for loop to print the gross_sales of employees again and again for 12 months
+                    for (int j = 1; j <= 12; ++j)
+                    {
+                        Console.WriteLine($"Enter the gross sales for employee {i} of month {j}: \n");
+                        int gross_sales = Convert.ToInt32(Console.ReadLine());
+
+                        totalGrossSales += gross_sales;
+
+                    }
+                    float Per_month_earnings = salary + (gross_tax * totalGrossSales);
+
+                    totalEarnings += totalGrossSales;
+                    Console.WriteLine($"The Earnings per month for employee {i} are: {Per_month_earnings}\n");
+                }
+
+                Console.WriteLine($"Total Earnings for all employees: {totalEarnings}\n");
+            }
+        }
+    }
+}
