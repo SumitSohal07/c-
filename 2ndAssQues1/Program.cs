62a00cae8c65ba6037dfc79f5046756c829801ea
SumitSohal07 <145088567+SumitSohal07@users.noreply.github.com>
2024-02-09 23:52:04 -0500

    Add files via upload
    created a program to calculate the salary of employees by using while loop only.

---
+++ 2ndAssQues1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class dental
+{
+    static void Main(string[] args)
+    {
+        //Declaration of variables
+
+        int employee = 1;
+        int user_employee;
+        const int salary = 1000;
+        int choose = 0;
+
+        // Message will be displayed to exit the program or continue with the program
+
+        Console.WriteLine($"Press 1 to exit the program or enter any other number to continue");
+        choose = Convert.ToInt32(Console.ReadLine());
+
+        while (choose != 1)
+        {
+            if (choose == 1)
+            {
+                Console.WriteLine("Goodbye!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Enter the number of employees");
+                user_employee = Convert.ToInt32(Console.ReadLine());
+
+                while (employee <= user_employee)
+                {
+                    Console.WriteLine($"The gross sales for employee {employee} for each month are:");
+
+                    double totalGrossSales = 0;
+                    double totalearnings = 0;
+                     
+                    int months = 1; // Reset months for each employee
+                    while (months <= 12)
+                    {
+                        Console.WriteLine($"Enter the gross sales of employee {employee} for month {months}");
+                        int gross_sales = Convert.ToInt32(Console.ReadLine());
+                        totalGrossSales = salary + (0.1 * gross_sales);
+                        totalearnings += totalGrossSales;
+                        months++;
+                    }
+
+                    Console.WriteLine($"Total earnings of the year for employee {employee} is : ${totalGrossSales}");
+                    Console.WriteLine(); 
+
+                    employee++;
+                }
+            }
+        }
+    }
+}
